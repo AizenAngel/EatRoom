e1a473e095e3dc694b7ab1bae822b885f943d481
Luka Vujčić <lukavujcic@hotmail.com>
2022-03-30 22:24:38 +0200

    Added Dish repository

--- EatRoom/Restaurants/Entities/Menu.cs
+++ EatRoom/Restaurants/Entities/Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Restaurants.Entities
+namespace Restaurants.API.Entities
 {
     public class Menu
     {
---
+++ EatRoom/Restaurants/Repositories/IDishRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Restaurants.API.Entities;
+namespace Restaurants.API.Repositories
+{
+    interface IDishRepository
+    {
+        Task<IEnumerable<Dish>> GetAllDishes();
+
+        Task<Dish> GetDish(string id);
+
+        Task CreateDish(Dish dish);
+
+        Task<bool> UpdateDish(Dish dish);
+
+        Task<bool> DeleteDish(string id);
+
+    }
+}
