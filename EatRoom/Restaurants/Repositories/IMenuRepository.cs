f9cd75cb4d58477403efc51bd5e17459db774314
Luka Vujčić <lukavujcic@hotmail.com>
2022-03-31 22:22:32 +0200

    Added some repositories

---
+++ EatRoom/Restaurants/Repositories/IMenuRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Restaurants.API.Entities;
+namespace Restaurants.API.Repositories
+{
+    interface IMenuRepository
+    {
+
+        Task<IEnumerable<Menu>> GetAllMenus();
+
+        Task<Menu> GetMenu(string id);
+
+        Task CreateMenu(Menu menu);
+
+        Task<bool> UpdateMenu(Menu menu);
+
+        Task<bool> DeleteMenu(string id);
+    }
+}
---
+++ EatRoom/Restaurants/Repositories/IRestaurantRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Restaurants.API.Entities;
+namespace Restaurants.API.Repositories
+{
+    interface IRestaurantRepository
+    {
+
+        Task<IEnumerable<Restaurant>> GetAllRestaurants();
+
+        Task<Restaurant> GetRestaurant(string id);
+
+        Task CreateRestaurant(Restaurant restaurant);
+
+        Task<bool> UpdateRestaurant(Restaurant restaurant);
+
+        Task<bool> DeleteRestauran(string id);
+    }
+}
