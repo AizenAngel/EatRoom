47b3de42cea400fea85b832f55e1798f4e003c6d
MarijaEric <marija.eric1997@gmail.com>
2022-04-22 22:34:21 +0200

    Finished implementation of domain layer in Ordering microservice.

---
+++ EatRoom/Ordering/Ordering.Domain/Entities/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿using Ordering.Domain.Common;
+using Ordering.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Entities
+{
+    public class OrderItem : EntityBase
+    {
+        public string ProductName { get; private set; }
+        public int ProductId { get; private set; }
+        public string PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+        public int Units { get; private set; } = 0;
+
+        public OrderItem(string productName, int productId, string pictureUrl, decimal price, int units)
+        {
+            ProductName = productName ?? throw new ArgumentNullException(nameof(productName));
+            ProductId = productId;
+            PictureUrl = pictureUrl ?? throw new ArgumentNullException(nameof(pictureUrl));
+            Price = price;
+            AddUnits(units);
+        }
+
+        public void AddUnits(int units)
+        {
+            int newUnits = Units + units;
+
+            if (newUnits <= 0)
+            {
+                throw new OrderingDomainException("Invalid number of units in OrderItem!");
+            }
+            Units = newUnits;
+        }
+
+        public decimal getTotalPriceItem()
+        {
+            return Price * Units;
+        }
+    }
+}
---
+++ EatRoom/Ordering/Ordering.Domain/ValueObjects/Addresses.cs
@@ -0,0 +1,38 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public class Addresses : ValueObject
+    {
+        /*Important informations about addresses in Serbia: Street name, number, zipcode, city, district*/
+
+        public string StreetName { get; private set; }
+        public string StreetNumber { get; private set; }
+        public int ZipCode { get; private set; }
+        public string CityName { get; private set; }
+        public string DistrictName { get; private set; }
+
+        public Addresses(string streetName, string streetNumber, int zipCode, string cityName, string districtName)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            StreetNumber = streetNumber ?? throw new ArgumentNullException(nameof(streetNumber));
+            ZipCode = zipCode;
+            CityName = cityName ?? throw new ArgumentNullException(nameof(cityName));
+            DistrictName = districtName ?? throw new ArgumentNullException(nameof(districtName));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return StreetName;
+            yield return StreetNumber;
+            yield return ZipCode;
+            yield return CityName;
+            yield return DistrictName;
+        }
+    }
+}
