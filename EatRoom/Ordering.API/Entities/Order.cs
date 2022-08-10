fe35ec957aadf1ee4e3bfe4d4af979be850041e1
Luka Vujčić <lukavujcic@hotmail.com>
2022-08-10 20:57:21 +0200

    Added entities

---
+++ EatRoom/Ordering.API/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Entities
+{
+    public class Order
+    {
+		public int UserId { get; set; }
+		public int RestarauntId { get; set; }
+		
+		public double Price { get; set; }
+	    public StateEnum State { get; set; }
+		public int DelivererId { get; set; }
+	}
+
+	public enum StateEnum
+    {
+		Preparing,
+		Delivering,
+		Delivered
+	}
+}
---
+++ EatRoom/Ordering.API/Entities/OrderDish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Entities
+{
+    public class OrderDish
+    {
+        public int OrderDishId { get; set; }
+        public int OrderId { get; set; }
+
+        public int DishId { get; set; }
+    }
+}
